9cd62d040638be3b2fee56a468af96e153903daf
Jyoti Shinde <kitto@Jyotis-MacBook-Pro.local>
2022-12-18 20:04:39 -0500

    added hardcoded authentication to menu component

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  // isUserLoggedIn: boolean = false;
 
-  ngOnInit(): void {
+  constructor(public hardcodedAuthenticationService: HardcodedAuthenticationService) { }
+
+  ngOnInit(){
+    // this.isUserLoggedIn = this.hardcodedAuthenticationService.isUserLoggedIn();
+
   }
+
+  
 
 }
