fa66f52327ad10b8f912ce3667f4fc58441be949
Jyoti Shinde <kitto@Jyotis-MacBook-Pro.local>
2022-12-18 22:00:43 -0500

    adding routegurad to all routes

--- src/app/service/route-guard.service.ts
+++ src/app/service/route-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { HardcodedAuthenticationService } from './hardcoded-authentication.service';
 
 @Injectable({
@@ -7,12 +7,14 @@ import { HardcodedAuthenticationService } from './hardcoded-authentication.servi
 })
 export class RouteGuardService implements CanActivate{
 
-  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
+  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService,
+    private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
     if(this.hardcodedAuthenticationService.isUserLoggedIn()){
       return true;
     }
+    this.router.navigate(["login"]);
     return false;
   }
 }
