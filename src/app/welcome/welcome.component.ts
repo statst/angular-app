13c135c50a9f010bb3ff3e55679b391337f0c1cc
Jyoti Shinde <kitto@Jyotis-MacBook-Pro.local>
2022-12-27 20:15:02 -0500

    calling welcome api on UI

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HelloWorldBean, WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -9,12 +10,31 @@ import { ActivatedRoute } from '@angular/router';
 export class WelcomeComponent implements OnInit {
 
   message: string = 'Hello Welcome to Lineup'
+  welcomeFromService:string= ''
   name: string = ''
 
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute,
+    private service:WelcomeDataService) { 
+
+  }
 
   ngOnInit(): void {
     this.name = this.route.snapshot.params['name'];
+  }
+  getWelcomeMessage(){
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfullResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+  }
+ 
+
+  handleSuccessfullResponse(response){
+    this.welcomeFromService = response.message;
+  }
+
+  handleErrorResponse(error){
+    this.welcomeFromService = error.error.message;
   }
 
 }
